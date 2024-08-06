19c9828b461086c6851caac2222cb07a7bff2f54
Raviraj <120179700+RavirajkPatil@users.noreply.github.com>
2024-08-06 19:40:42 +0530

    Add files via upload

---
+++ 01 Basics/01-variables.js
@@ -0,0 +1,13 @@
+const CustomerId = 12354    // It is onetime declaration does not changable in the whole application.
+var custEmail = "ravi.p@test.com"
+let custName = "Raviraj"
+custLastName = "Patil"
+let custState  // Most use in js now 'let'
+
+console.log(CustomerId);
+console.table([custEmail,custName,custLastName,custState])
+
+
+/*
+To run " node 01-variables.js "
+*/
---
+++ 01 Basics/Test.js
@@ -0,0 +1,11 @@
+/*let str = "Hello Javascipt"
+let result = str.replace("Javascipt","World")
+console.log(result)
+
+console.log(typeof(result))
+
+let a='  ravi '
+let b = ' patil'
+let result1 = a+b
+console.log(result1.trim())
+*/
