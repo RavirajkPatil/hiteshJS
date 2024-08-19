66c4cd04186dbf8a41e8306943ffbb351b1730a8
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-08-19 20:10:18 +0530

    strings types added

---
+++ 01 Basics/05-String.js
@@ -0,0 +1,35 @@
+let Name = 'Ravi'
+let LName = new String('raj') // if you want to see string types then paste this line in browser console and call it.
+
+//console.log(Name+" "+LName+" Patil")
+console.log(`My name is ${Name} ${LName} Patil`);   // interpolation symbol in js
+/*
+console.log(LName[0]);
+console.log(LName.__proto__);
+console.log(LName.length);
+console.log(LName.toUpperCase());
+console.log(LName.charAt(2));       //  which char position
+console.log(LName.indexOf('a'));       //  which index position
+
+console.log(LName.slice(0,2));  // gives inbetween character
+console.log(LName.slice(-4,1));  // gives inbetween character
+console.log(LName.substring(0,2)); 
+*/
+/*
+const newString = '   Ridu   '
+console.log(newString.trim())
+console.log(newString.trimEnd())
+console.log(newString.trimStart())
+*/
+
+/*
+let url = "https://habasit.com/en/Products/Monolithic%20Belts"
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('habasit1'));
+*/
+
+let str = 'raviraj-rajendra-patil'
+console.log(str.split('-'));
+
+
