c01bafc96fa7ec634a9ae84e88b3de53cfde412a
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-08-19 19:38:17 +0530

    adding primitive and nonPremitive

--- 01 Basics/Datatype-summary.js
+++ 01 Basics/Datatype-summary.js
@@ -10,12 +10,12 @@
 
     const id = Symbol('123')    //symbol
     const anotherId = Symbol('123')     //symbol
-    console.log(id == anotherId)
+    //console.log(id == anotherId)
 
     const bigNumber = 456415485456468854654823876n      //bigint
     //console.log(bigNumber)
 
-    console.log(typeof id)
+    //console.log(typeof id)
 
 /*
     Non-Premitive:(Reference)
@@ -33,4 +33,28 @@
         console.log("Hello world")
     }
 
-    console.log(typeof myFunction)
+    //console.log(typeof myFunction)
+
+    //=======================================================
+    
+    //Stack : Store Primitive datatypes.
+    //  We get copy of values.
+    let FirstName = "Raviraj"
+    let NickName = FirstName
+    NickName = "Pruthviraj"
+
+    console.log(FirstName);
+    console.log(NickName)
+    
+    //Heap : Store non-premitive datatypes.
+    //  We get reference values.
+    let UserOne = {
+        email: 'raviraj@gmai.com',
+        upi:'ravi@ybl'
+    }
+    //console.log(UserOne.email)
+    let UserTwo = UserOne
+    UserTwo.email = "pruth@gmail.com"
+    
+    console.log(UserOne.email)
+    console.log(UserTwo.email)
