b88eb71d7ca541951cb3a87f49a20862b30a4cd4
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-08-21 18:02:15 +0530

    Object literals

---
+++ 02_Basics/03_Object.js
@@ -0,0 +1,32 @@
+// singletop
+//Object.create()
+
+// Object literals
+
+let mySym = Symbol("MySym1")
+
+let jsUser = {
+    name : "Raviraj",
+    [mySym] : "jsSymbol",
+    "email": "ravi@google.com",
+    age:30
+}
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser[mySym]);
+
+jsUser.email = "riddhesh@hotmail.com"
+//Object.freeze(jsUser)
+jsUser.email = "riddhesh@gmail.com"
+
+//console.log(jsUser);
+
+jsUser.jsGretings = function() {
+    console.log("Hello js");
+}
+jsUser.jsGretingsTwo = function() {
+    console.log(`Hello js user ,${jsUser.name}`);
+}
+//console.log(jsUser.jsGretings);     // [Function (anonymous)]
+console.log(jsUser.jsGretings());   //Hello js
+console.log(jsUser.jsGretingsTwo());    // Hello js user ,Raviraj
