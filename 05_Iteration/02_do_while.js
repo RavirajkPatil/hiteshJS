616800d515a6649db5a0881100c98a9ddc874dbc
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-08-27 16:12:08 +0530

    do while implemented

---
+++ 05_Iteration/02_do_while.js
@@ -0,0 +1,44 @@
+/* while loop = 
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`)
+    index = index + 2
+} */
+// Output:
+        // value of index is 0
+        // value of index is 2
+        // value of index is 4
+        // value of index is 6
+        // value of index is 8
+        // value of index is 10
+//------------------------------
+/*
+let myArray = ["Shaktiman","Batman","Spiderman"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+    Output :
+        Value is Shaktiman
+        Value is Batman
+        Value is Spiderman
+*/
+//----------- do..while.-----------------
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+// Output: 
+        // Score is 1
+        // Score is 2
+        // Score is 3
+        // Score is 4
+        // Score is 5
+        // Score is 6
+        // Score is 7
+        // Score is 8
+        // Score is 9
+        // Score is 10
