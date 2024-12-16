0745a02bf8b97a6475234ab0fe021b67bc0523ca
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-12-16 11:05:18 +0530

    name change of folde

---
+++ 01_Basics/02-datatypes.js
@@ -0,0 +1,16 @@
+/*
+Premitive Datatypes:
+Number =>  2 power of 52
+bigInit
+string => ""
+bool => true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+Non-premitive datatypes:
+object
+    */
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object
---
+++ 01_Basics/03-DatatypeConversion.js
@@ -0,0 +1,29 @@
+"use strict" // Treat all js code in newer version
+
+let count = "33avd"
+
+//console.log(typeof count)
+//console.log(typeof(count))
+
+let number = Number(count)
+//console.log(number)     //NaN = Not a Number.
+/*
+    "33" => 33
+    "33abc" => NaN
+    true => 1 ; false => */
+
+//------------------------
+let IsLoggedIn = 1  // true
+let IsLoggedIn1 = ""  // false
+let IsLoggedIn2 = "Raviraj"  // true
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//---------------------------------
+
+let someNumber=33
+let convertSomeNumber= String(someNumber)
+
+console.log(convertSomeNumber)
+console.log(typeof convertSomeNumber)
